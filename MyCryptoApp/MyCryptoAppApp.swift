db06aa584fb3f4a180fb1c9f3a357d10793711d7
Swapnil Katwe <swapnil.katwe111@gmail.com>
2024-04-11 12:34:35 +0200

    Initial Commit

---
+++ MyCryptoApp/MyCryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCryptoAppApp.swift
+//  MyCryptoApp
+//
+//  Created by Swapnil Katwe on 11/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MyCryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
