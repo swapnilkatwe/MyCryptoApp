5d3eeda4c6dba0c0d00f31d0ef59b4e70cb06535
Swapnil Katwe <swapnil.katwe111@gmail.com>
2024-04-17 15:22:41 +0200

    Added API having endpoint global. It is used to get marketData and show statistics on HomeView below searchbar. Also minor speling mistake updated for asPercentString

--- MyCryptoApp/Core/Home/Views/CoinRowView.swift
+++ MyCryptoApp/Core/Home/Views/CoinRowView.swift
@@ -56,7 +56,7 @@ extension CoinRowView {
                 .bold()
                 .foregroundStyle(Color.theme.accent)
             
-            Text(coin.priceChangePercentage24H?.asPercenstString() ?? "")
+            Text(coin.priceChangePercentage24H?.asPercentString() ?? "")
                 .foregroundStyle(
                     (coin.priceChangePercentage24H ?? 0) >= 0 ?
                     Color.theme.green :
