0f8db559f2cb91a881c5d45e41b39750429aa00f
Swapnil Katwe <swapnil.katwe111@gmail.com>
2024-04-25 18:07:27 +0200

    Launch Screen using Storyboard. Loading page same as launch screen with animation.

--- MyCryptoApp/MyCryptoAppApp.swift
+++ MyCryptoApp/MyCryptoAppApp.swift
@@ -10,18 +10,28 @@ import SwiftUI
 @main
 struct MyCryptoAppApp: App {
     @StateObject private var vm = HomeViewModel()
-    
+    @State private var showLaunchView: Bool = true
     init() {
         UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor(Color.theme.accent)]
         UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor(Color.theme.accent)]
     }
     var body: some Scene {
         WindowGroup {
-            NavigationStack {
-                HomeView()
-                    .toolbar(.hidden)
+            ZStack {
+                NavigationStack {
+                    HomeView()
+                        .toolbar(.hidden)
+                }
+                .environmentObject(vm)
+                
+                ZStack {
+                    if showLaunchView {
+                        LaunchView(showLaunchView: $showLaunchView)
+                            .transition(.move(edge: .leading))
+                    }
+                }
+                .zIndex(2.0)// Controls Overlapping views
             }
-            .environmentObject(vm)
         }
     }
 }
